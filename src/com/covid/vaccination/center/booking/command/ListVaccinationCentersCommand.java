f4994a2de332a9190d959a3ead264240f850e70f
Prachi Varshney <prachi25varshney@gmail.com>
2022-07-03 15:07:52 +0530

    Add functionality to list vaccination centers

--- src/com/covid/vaccination/center/booking/command/ListVaccinationCentersCommand.java
+++ src/com/covid/vaccination/center/booking/command/ListVaccinationCentersCommand.java
@@ -1,8 +1,12 @@
 package com.covid.vaccination.center.booking.command;
 
 import com.covid.vaccination.center.booking.dto.request.VaccinationCenterList;
+import com.covid.vaccination.center.booking.service.UserService;
 
 public class ListVaccinationCentersCommand implements ICommand {
+
+    UserService userService = new UserService();
+
     @Override
     public <T> T create(String[] input) {
         return (T) new VaccinationCenterList(input[1]);
@@ -10,6 +14,6 @@ public class ListVaccinationCentersCommand implements ICommand {
 
     @Override
     public <T> void execute(T input) {
-
+        userService.listVaccinationCenters((VaccinationCenterList) input);
     }
 }
--- src/com/covid/vaccination/center/booking/service/UserService.java
+++ src/com/covid/vaccination/center/booking/service/UserService.java
@@ -1,9 +1,12 @@
 package com.covid.vaccination.center.booking.service;
 
 import com.covid.vaccination.center.booking.dto.request.User;
+import com.covid.vaccination.center.booking.dto.request.VaccinationCenterList;
+import com.covid.vaccination.center.booking.dto.response.Center;
 import com.covid.vaccination.center.booking.exceptions.UserAlreadyRegistered;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserService {
@@ -15,5 +18,12 @@ public class UserService {
         if (users.containsKey(userId))
             throw new UserAlreadyRegistered("User Already Exists");
         users.put(userId, user);
+    }
+
+    public void listVaccinationCenters(VaccinationCenterList vaccinationCenterList) {
+        String district = vaccinationCenterList.getDistrictName();
+        List<Center> centerList = AdminService.vaccinationCenterListInDistrict.get(district);
+        centerList.forEach(x -> x.getCapacityList().forEach(y -> System.out.println(x.getCenterId() + " " + y.getDay()
+                + " " + y.getCapacity())));
     }
 }
